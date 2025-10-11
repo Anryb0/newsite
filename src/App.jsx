3acbf6fb4089c635b944e52f7dfd5e4acef4dd0b
Anryb0 <you@example.com>
2025-10-11 13:26:56 +0300

    getuser works

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import Header from './components/Header.jsx'
 
 function App() {	
   return (
     <div className="todoapp stack-large">
-		
+		<Header />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,47 @@
+import { useEffect, useState } from 'react';
 import './Header.css';
 
 function Header()
 {
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", "http://94.183.234.114/server/getuser.php");
-	xhr.onload = function() {
-        if (xhr.status == 200) {
-			responce = JSON.parse(xhr.responseText);
-			if (!responce.loggedin){
-				
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		let user;
+		let xhr = new XMLHttpRequest();
+		xhr.open("POST", "http://94.183.234.114/server/getuser.php");
+		xhr.send();
+		xhr.onload = function() {
+			if (xhr.status == 200) {
+				let responce = JSON.parse(xhr.responseText);
+				if (responce.loggedin){
+					setUser(responce);
+				}
+				else {
+					setUser(null);
+				}
+				setLoading(false);
 			}
 			else {
-				
+				console.log('ошибка');
+				setLoading(false);
 			}
 		}
-		else {
-			console.log('ошибка')
-		}
-	}
+	},[]);
 	return (
-		<div id='lm'>
-			<div>
-				<div class="logo"><img src="../pic/shop.png" id='logo'></div>
-				<div id='pagename'><b>Computer shop</b></div>
+		<div id='header'>
+			<div id='lm'>
+				<div>
+					<div className="logo"><img src="/shop.png" id='logo' /></div>
+					<div id='pagename'><b>Computer shop</b></div>
+				</div>
+				{loading ? (
+					<span id='lau'>Загрузка...</span>
+				) : user ? (
+					<a id='lau' href="/profile">{user.username}</a>
+				) : (
+					<a id='lau' href="/register">Регистрация / Вход</a>
+				)}
 			</div>
-			
 		</div>
 	)
 }
