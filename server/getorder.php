4593bfd6a3330a5322aba4be46010cab1438c450
Your Name <you@example.com>
2025-10-30 11:49:46 +0300

    asz

---
+++ server/getorder.php
@@ -0,0 +1,49 @@
+<?php
+	if(!isset($_POST['id'])){
+		echo json_encode(['success'=>false,'message'=>'Нет данных']);
+		exit;
+	}
+	include 'connect.php';
+	if(!isset($_SESSION['id'])){
+		echo json_encode(['success'=>false,'message'=>'Вы не авторизованы']);
+		$conn->close();
+		exit;
+	}
+	$stmt = $conn->prepare('select s.status, s.time, l.name, l.address from shop_orders s left join shop_locations l on l.id = s.shop_id where id = ?');
+	$stmt->bind_param('i', $_POST['id']);
+	$stmt->execute();
+	$result=$stmt->get_result();
+	if($result->num_rows == 0){
+		echo json_encode(['success'=>false,'message'=>'Заказ не найден =(']);
+		$stmt->close();
+		$conn->close();
+		exit;
+	}
+	$order = $result->fetch_assoc();
+	$stmt->close();
+	if($order['user_id'] != $_SESSION['id']){
+		echo json_encode(['success'=>false,'message'=>'Этот заказ был оформлен с другого аккаунта =(']);
+		$conn->close();
+		exit;
+	}
+	$stmt = $conn->prepare('select c.product_id, c.q, c.price, g.name, g.photo_url from shop_cart c left join shop_goods g on c.product_id = g.id where c.order_id = ?');
+	$stmt->bind_param('i',$_POST['id']);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$data = [];
+	while($row = $result->fetch_assoc()){
+		$data[] = [
+			'product_id' => $row['product_id'],
+			'q' => $row['q'],
+			'price' => $row['price'],
+			'name' => $row['name'],
+			'photo_url' => $row['photo_url']
+		];
+	}
+	echo json_encode(['success'=>true,'data'=> $data, 'order'=>$order]);
+	$stmt->close();
+	$conn->close();
+	exit;
+
+
+?>
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -7,11 +7,67 @@ import './Order.css';
 
 function Order(){
 	const[loading, setLoading] = useState(true);
+	const[show,setShow] = useState(false);
+	const[closing,setClosing] = useState(false);
+	const[error,setError] = useState('');
+	const[err,setErr] = useState(false);
+	const[data,setData] = useState(null);
+	const[order,setOrder] = useState(null);
+	const location = useLocation();
+	const queryParams = new URLSearchParams(location.search);
+	const order = queryParams.get('id');
+	
+	function closemodal(){
+		setClosing(true);
+		setTimeout(() => {
+			setShow(false);
+			setClosing(false);
+		}, 300);
+	}
+	function openmodal(t, err){
+		setErr(err);
+		setError(t);
+		setShow(true);
+	}
+	
+	useEffect(() => {
+		let formData = new FormData();
+		formData.append('id',order);
+		let xhr = new XMLHttpRequest();
+		xhr.open('POST', 'http://94.183.234.114/server/getorder.php');
+		xhr.send(formData);
+		xhr.onload = function(){
+			if(xhr.status == 200){
+				let response = JSON.parse(xhr.responseText));
+				if(response.success){
+					setData(response.data);
+					setOrder(response.order);
+				}
+				else{
+					openmodal(response.message, true);
+				}
+			}
+			else{
+				openmodal('Ошибка соединения: ' + xhr.status, true);
+			}
+			setLoading(false);
+		}
+	}, [order]);
 	return(
 		<>
 			<Header name='Computer shop' search={true}/>
+			{show && (
+				<Erwin text={error} closemodal={closemodal} closing={closing}/>
+			)}
 			<main>
-			{loading ? (<div className='spinner'></div>) : (<div></div>)}
+			{loading ? (<div className='spinner'></div>) : (
+				<>
+					<p id='toptext'>Заказ №{order.id}</p>
+					<p className='orderprop'>Заказ оформлен: {order.time.split(':').slice(0, 2).join(':')}</p>
+					<p className='orderprop'>Статус заказа: {order.status}</p>
+					<p className='orderprop'></p>
+				</>
+			)}
 			</main>
 			<Footer />
 		</>
